964fa0b11a581340d23c93e59318e4d2af82976c
Wael Orraby <orraby2000@gmail.com>
2023-11-16 21:08:16 +0100

    Added Initial classes

--- GameDevProject/obj/Debug/net6.0-windows/GameDevProject.AssemblyInfo.cs
+++ GameDevProject/obj/Debug/net6.0-windows/GameDevProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameDevProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8852304ff692bb631f4d9af8eea45f92e934d30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameDevProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameDevProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
