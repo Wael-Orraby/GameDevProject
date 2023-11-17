e4fb5b62311d19a4da71fc71fa968a13c3fe6c09
Wael-Orraby <orraby2000@gmail.com>
2023-11-17 15:23:23 +0100

    Werk aan Player-klasse: Basisfunctionaliteit toegevoegd, nog enkele e…

--- GameDevProject/Game/Player.cs
+++ GameDevProject/Game/Player.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,184 @@ namespace GameDevProject.Game
 {
     public class Player
     {
+        // Animaties
+        private Animation idleAnimation;
+        private Animation runAnimation;
+        private Animation jumpAnimation;
+        private Animation celebrateAnimation;
+        private Animation dieAnimation;
+        private SpriteEffects flip = SpriteEffects.None;
+        private AnimationPlayer sprite;
+
+        // Geluiden
+        private SoundEffect killedSound;
+        private SoundEffect jumpSound;
+        private SoundEffect fallSound;
+
+        public Level Level
+        {
+            get { return level; }
+        }
+        Level level;
+
+        public bool IsAlive
+        {
+            get { return isAlive; }
+        }
+        bool isAlive;
+
+        // Fysische toestand
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+        Vector2 position;
+
+        private float previousBottom;
+
+        public Vector2 Velocity
+        {
+            get { return velocity; }
+            set { velocity = value; }
+        }
+        Vector2 velocity;
+
+        // Constanten voor het controleren van horizontale beweging
+        private const float MoveAcceleration = 13000.0f;
+        private const float MaxMoveSpeed = 1750.0f;
+        private const float GroundDragFactor = 0.48f;
+        private const float AirDragFactor = 0.58f;
+
+        // Constanten voor het controleren van verticale beweging
+        private const float MaxJumpTime = 0.35f;
+        private const float JumpLaunchVelocity = -3500.0f;
+        private const float GravityAcceleration = 3400.0f;
+        private const float MaxFallSpeed = 550.0f;
+        private const float JumpControlPower = 0.14f;
+
+        // Inputconfiguratie
+        private const float MoveStickScale = 1.0f;
+        private const float AccelerometerScale = 1.5f;
+        private const Buttons JumpButton = Buttons.A;
+
+        /// <summary>
+        /// Geeft aan of de voeten van de speler al dan niet op de grond staan.
+        /// </summary>
+        public bool IsOnGround
+        {
+            get { return isOnGround; }
+        }
+        bool isOnGround;
+
+        /// <summary>
+        /// Huidige invoer van gebruikersbeweging.
+        /// </summary>
+        private float movement;
+
+        // Springstatus
+        private bool isJumping;
+        private bool wasJumping;
+        private float jumpTime;
+
+        private Rectangle localBounds;
+        /// <summary>
+        /// Geeft een rechthoek die deze speler begrenst in wereldruimte.
+        /// </summary>
+        public Rectangle BoundingRectangle
+        {
+            get
+            {
+                int left = (int)Math.Round(Position.X - sprite.Origin.X) + localBounds.X;
+                int top = (int)Math.Round(Position.Y - sprite.Origin.Y) + localBounds.Y;
+
+                return new Rectangle(left, top, localBounds.Width, localBounds.Height);
+            }
+        }
+
+        /// <summary>
+        /// Cre ert een nieuwe speler.
+        /// </summary>
+        public Player(Level level, Vector2 position)
+        {
+            this.level = level;
+
+            LoadContent();
+
+            Reset(position);
+        }
+
+        /// <summary>
+        /// Laadt het spritesheet en geluiden van de speler.
+        /// </summary>
+        public void LoadContent()
+        {
+            // Laad geanimeerde textures.
+            // !!!!!!!!!!!!!!!!!! Nog te doen !!!!!!!!!!!!!!
+
+            // Bereken begrenzingen binnen de grootte van de texture .           
+            // !!!!!!!!!!!!!!!!!!Nog te doen !!!!!!!!!!!!!!
+
+            // Laad geluiden.            
+            // !!!!!!!!!!!!!!!!!! Nog te doen !!!!!!!!!!!!!!
+        }
+
+        /// <summary>
+        /// Reset de speler naar het leven.
+        /// </summary>
+        /// <param name="position">De positie waar de speler tot leven komt.</param>
+        public void Reset(Vector2 position)
+        {
+            Position = position;
+            Velocity = Vector2.Zero;
+            isAlive = true;
+            sprite.PlayAnimation(idleAnimation);
+        }
+
+        /// <summary>
+        /// Handelt invoer af, voert natuurkunde uit en animeert de speler sprite.
+        /// </summary>
+        public void Update(
+            GameTime gameTime,
+            KeyboardState keyboardState,
+            GamePadState gamePadState,
+            AccelerometerState accelState,
+            DisplayOrientation orientation)
+        {
+            GetInput(keyboardState, gamePadState, accelState, orientation);
+
+            ApplyPhysics(gameTime);
+
+            if (IsAlive && IsOnGround)
+            {
+                if (Math.Abs(Velocity.X) - 0.02f > 0)
+                {
+                    sprite.PlayAnimation(runAnimation);
+                }
+                else
+                {
+                    sprite.PlayAnimation(idleAnimation);
+                }
+            }
+
+            // Clear input.
+            movement = 0.0f;
+            isJumping = false;
+        }
+
+        /// <summary>
+        /// Tekent de geanimeerde speler.
+        /// </summary>
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            // Keert de sprite om zodat deze de kant op kijkt waarheen we bewegen.
+            if (Velocity.X > 0)
+                flip = SpriteEffects.FlipHorizontally;
+            else if (Velocity.X < 0)
+                flip = SpriteEffects.None;
+
+            // Teken die sprite.
+            sprite.Draw(gameTime, spriteBatch, Position, flip);
+        }
     }
 }
