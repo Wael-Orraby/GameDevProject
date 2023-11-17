5dcd82cbeeacfb7c9ab5dee9fc070824195762cf
marouane013 <91462790+marouane013@users.noreply.github.com>
2023-11-17 14:42:09 +0100

    klasse gemaakt voor de tiles

--- GameDevProject/Game/Tile.cs
+++ GameDevProject/Game/Tile.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
-namespace GameDevProject.Game
+namespace Platformer2D
 {
-    public class Tile
+
+    /// deze enum bepaald of een blokje collision moet hebben ofniet
+    enum TileCollision
     {
+        /// dit is een doorzichtige blokje waar je door heen kunt lopen
+        Passable = 0,
+
+        /// door deze tegel kan je niet heen bewegen
+        Impassable = 1,
+
+        /// <summary>
+        /// je kan hier op staan of via onder doorheen komen
+        /// </summary>
+        Platform = 2,
+    }
+
+    /// <summary>
+    /// deze klas bepaald het gedrag van een blokje het gedrag van een blokje
+    /// </summary>
+    struct Tile
+    {
+        public Texture2D Texture;
+        public TileCollision Collision;
+
+        public const int Width = 40;
+        public const int Height = 32;
+
+        public static readonly Vector2 Size = new Vector2(Width, Height);
+
+        public Tile(Texture2D texture, TileCollision collision)
+        {
+            Texture = texture;
+            Collision = collision;
+        }
     }
 }
