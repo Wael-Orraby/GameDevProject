301a59042fc81be24893232eeb15df6d586e1888
marouane013 <91462790+marouane013@users.noreply.github.com>
2023-11-17 16:03:57 +0100

    begin gemaakt aan het leveldesign van level 1

--- GameDevProject/Game/Level.cs
+++ GameDevProject/Game/Level.cs
@@ -1,12 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+using System.IO;
+using Microsoft.Xna.Framework.Input;
+using GameDevProject.Game;
 
 namespace GameDevProject.Game
 {
-    public class Level
+    /// <summary>
+    /// deze klas maakt een grid van alle tiles met een 2d array
+    /// </summary>
+    public class Level : IDisposable
     {
+        // 2d array maken wat de map gaat voorstellen
+        private Tile[,] tiles;
+        private Texture2D[] layers;
+        // aantal entiteiten in het spel
+        private const int EntityLayer = 2;
+
+        // de player aanroepen die in de map gaat komen
+        public Player Player
+        {
+            get { return player; }
+        }
+        Player player;
+
+        private List<Gem> gems = new List<Gem>();
+        private List<Enemy> enemies = new List<Enemy>();
+
+        // belangrijke locaties in de map        
+        private Vector2 start;
+        private Point exit = InvalidPosition;
+        private static readonly Point InvalidPosition = new Point(-1, -1);
+
+        // Level game state.
+        private Random random = new Random(354668);
+
+        public int Score
+        {
+            get { return score; }
+        }
+        int score;
+
+        public bool ReachedExit
+        {
+            get { return reachedExit; }
+        }
+        bool reachedExit;
+
+        public TimeSpan TimeRemaining
+        {
+            get { return timeRemaining; }
+        }
+        TimeSpan timeRemaining;
+
+        private const int PointsPerSecond = 5;
+
+        // Level content.        
+        public ContentManager Content
+        {
+            get { return content; }
+        }
+        ContentManager content;
+
+        private SoundEffect exitReachedSound;
     }
-}
