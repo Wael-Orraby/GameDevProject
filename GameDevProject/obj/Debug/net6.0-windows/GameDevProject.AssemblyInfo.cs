d1fb730a2403cae06a7c6da428c224900a9d69c6
marouane013 <91462790+marouane013@users.noreply.github.com>
2023-11-30 20:45:11 +0100

    start Level klasse

--- GameDevProject/obj/Debug/net6.0-windows/GameDevProject.AssemblyInfo.cs
+++ GameDevProject/obj/Debug/net6.0-windows/GameDevProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameDevProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8663dc8bb779c0bd376bcde12495fda017dfcabf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameDevProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameDevProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
