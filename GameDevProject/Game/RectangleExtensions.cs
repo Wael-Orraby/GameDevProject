a2a33f4e74f3cd639219c6490b1b390c2f6445ce
marouane013 <91462790+marouane013@users.noreply.github.com>
2023-12-08 15:07:47 +0100

    rectangle extension klasse gemaakt

--- GameDevProject/Game/RectangleExtensions.cs
+++ GameDevProject/Game/RectangleExtensions.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input.Touch;
 
 namespace GameDevProject.Game
 {
-    public class RectangleExtensions
+    public static class TouchCollectionExtensions
     {
+        /// bepaald of er contact is met het scherm
+        /// returnt TRUE als er contact is tijdens het bewegen en returnt false als er geen contact is
+        public static bool AnyTouch(this TouchCollection touchState)
+        {
+            foreach (TouchLocation location in touchState)
+            {
+                if (location.State == TouchLocationState.Pressed || location.State == TouchLocationState.Moved)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
