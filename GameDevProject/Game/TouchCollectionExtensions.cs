52b7fec9459d28cfbbc0a24f6bd013bc03c17d69
marouane013 <91462790+marouane013@users.noreply.github.com>
2023-12-08 15:21:55 +0100

    touch collectie gemaakt

--- GameDevProject/Game/TouchCollectionExtensions.cs
+++ GameDevProject/Game/TouchCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Input.Touch;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,20 @@ using System.Threading.Tasks;
 
 namespace GameDevProject.Game
 {
-    public class TouchCollectionExtensions
+    public static class TouchCollectionExtensions
     {
+        /// bepaald of er contact is met het scherm
+        /// returnt TRUE als er contact is tijdens het bewegen en returnt false als er geen contact is
+        public static bool AnyTouch(this TouchCollection touchState)
+        {
+            foreach (TouchLocation location in touchState)
+            {
+                if (location.State == TouchLocationState.Pressed || location.State == TouchLocationState.Moved)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
