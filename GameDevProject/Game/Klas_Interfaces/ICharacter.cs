fb508cac3eccf0482eae63ede6d2d1666f4240cd
marouane013 <91462790+marouane013@users.noreply.github.com>
2023-12-18 10:59:05 +0100

    interface voor de enemy en player

---
+++ GameDevProject/Game/Klas_Interfaces/ICharacter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevProject.Game.Klas_Interfaces
+{
+    internal interface Interface1
+    {
+    }
+}
