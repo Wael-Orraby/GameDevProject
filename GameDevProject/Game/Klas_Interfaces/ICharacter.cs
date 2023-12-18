81d9c7d6e775c37bc5276684d626e74fa0e5d109
marouane013 <91462790+marouane013@users.noreply.github.com>
2023-12-18 11:00:33 +0100

    Update ICharacter.cs

--- GameDevProject/Game/Klas_Interfaces/ICharacter.cs
+++ GameDevProject/Game/Klas_Interfaces/ICharacter.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace GameDevProject.Game.Klas_Interfaces
 {
-    internal interface Interface1
-    {
-    }
+      public interface ICharacter
+   {
+       Level Level { get; }
+
+       Vector2 Position { get; }
+
+       Rectangle BoundingRectangle { get; }
+
+       void LoadContent();
+
+
+       void Update(GameTime gameTime, KeyboardState keyboardState, GamePadState gamePadState, AccelerometerState accelState, DisplayOrientation orientation);
+
+       void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+   }
 }
