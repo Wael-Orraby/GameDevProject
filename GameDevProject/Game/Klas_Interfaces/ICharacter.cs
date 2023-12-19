ec50e0c2b76aebf8dbe51676fb254d7fb1fc4834
Wael Orraby <99222460+Wael-Orraby@users.noreply.github.com>
2023-12-19 14:51:48 +0100

    Update ICharacter.cs

--- GameDevProject/Game/Klas_Interfaces/ICharacter.cs
+++ GameDevProject/Game/Klas_Interfaces/ICharacter.cs
@@ -13,12 +13,5 @@ namespace GameDevProject.Game.Klas_Interfaces
        Vector2 Position { get; }
 
        Rectangle BoundingRectangle { get; }
-
-       void LoadContent();
-
-
-       void Update(GameTime gameTime, KeyboardState keyboardState, GamePadState gamePadState, AccelerometerState accelState, DisplayOrientation orientation);
-
-       void Draw(GameTime gameTime, SpriteBatch spriteBatch);
    }
 }
